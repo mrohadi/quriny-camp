5a71eaaf47fe0822387e5a19a6cf3d41b33c79b6
Muhammad Rohadi <muhammadrohadi03@gmail.com>
2020-07-29 22:24:36 +0700

    v2 - using mongodb and implementing restful route

--- README.md
+++ README.md
@@ -14,6 +14,29 @@ This app is providing a camp ground, so the use can search some of campground th
 - Setup route to show form
 - Add basic unstyled form
 - Add a better header/title
-- Make campgrounds page display  a grid
+- Make campgrounds page display a grid
 - Add a navbar to all templates
 - Style the new campground form
+
+### V2
+
+- Install and configurate mongoose
+- Setup campground model
+- Use campground model inside of our route
+- Add description to our campground model
+- Show db.collections.drop()
+- Add a new route/template
+
+> RESTFUL ROUTES
+
+In RESTFul routes, order is really matter
+
+|  Naem  |      Path      | HTTP Verb |                    Description                     |
+| :----: | :------------: | :-------: | :------------------------------------------------: |
+| INDEX  |     /dogs      |    GET    |             Display a list of all dog              |
+|  NEW   |   /dogs/new    |    GET    |          Dispaly a form to make a new dog          |
+| CREATE |     /dogs      |   POST    |              Add new dog to data base              |
+|  SHOW  |   /dogs/:id    |    GET    |              Show info about one dog               |
+|  EDIT  | /dogs/:id/edit |    GET    |             Show edit form for one dog             |
+| UPDATE |   /dogs/:id    |    PUT    | Update a particular dog, and redirect to somewhere |
+| DELETE |   /dogs/:id    |   DELTE   | Delte a particular dog, and redirect to somewhere  |
--- index.js
+++ index.js
@@ -1,73 +1,90 @@
 /* ==================== IMPORT PACKAGES ==================== */
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 // const path = require('path');
 // const favicon = require('express-favicon');
 const app = express();
 const port = process.env.PORT || 3000;
 
 /* ==================== EXPRESS CONFIGURATION ==================== */
-// to parser the req.body data 
+// to parser the req.body data
 app.use(bodyParser.urlencoded({ extended: true }));
 // set the page, so we don't need to include .ejs in our render response
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-/* ==================== ROUTES ==================== */
-const campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    name: "Granit Heel",
-    image:
-      "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    name: "Mountain Rest",
-    image:
-      "https://images.unsplash.com/photo-1520095972714-909e91b038e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    name: "Granit Heel",
-    image:
-      "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    name: "Mountain Rest",
-    image:
-      "https://images.unsplash.com/photo-1520095972714-909e91b038e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  },
-];
+/* ==================== MONGOOSE ==================== */
+// connect to mongodb
+mongoose
+  .connect("mongodb://localhost:27017/quriny-camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+// schema setup
+let campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+});
+// compile into a model (collections)
+let Campground = mongoose.model("Campground", campgroundSchema);
 
+/* ==================== ROUTES ==================== */
 // root route
 app.get("/", (req, res) => {
   res.render("home");
 });
-// show campgrounds route
+// Index - show campgrounds route
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  // get all campground from data base
+  Campground.find({}, (err, campgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // render the campgrounds
+      res.render("campgrounds", { campgrounds: campgrounds });
+    }
+  });
 });
-// post a new campground route
-app.post('/campgrounds', (req, res) => {
+// Create - post a new campground route
+app.post("/campgrounds", (req, res) => {
   // get data from form and add it to campground array
-  const { name, image } = req.body
-  const newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  // redirect back to campgrounds page
-  res.redirect('/campgrounds')
-})
-// route for show post new campground page
-app.get('/campgrounds/new', (req, res) => {
-  res.render('newCampground');
-})
+  const { name, image, description } = req.body;
+  const newCampground = { name: name, image: image, description: description };
+  // create a new campground and save it to data base
+  Campground.create(newCampground, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to campgrounds page
+      res.redirect("/campgrounds");
+    }
+  });
+});
+// New - route for show post new campground page
+app.get("/campgrounds/new", (req, res) => {
+  res.render("newCampground");
+});
+// Show - Show info about one campground
+app.get("/campgrounds/:id", (req, res) => {
+  const id = req.params.id;
+  // find the campground with provided ID
+  Campground.findById(id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // render show template with that campground
+      res.render("infoCampground", { campground: foundCampground });
+    }
+  });
+});
 
 /* ==================== LISTENING TO THE SERVER ==================== */
 app.listen(port, () => {
